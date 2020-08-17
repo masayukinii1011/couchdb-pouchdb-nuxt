bda40769beb715c9ec292fcaf2ff62ba21e80f83
masayukinii1011 <masayukinii1011@gmail.com>
2020-08-17 22:45:13 +0900

    書き込みと呼び出し

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,52 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        couchdb-pouchdb-nuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <input type="text" v-model="newTitle" />
+    <input type="text" v-model="newId" />
+    <button @click="add()">add</button>
+    <button @click="test()">test</button>
+    <div v-for="(row, index) in rows" :key="index">{{row.id}} {{row.doc.title}}</div>
   </div>
 </template>
 
 <script>
-export default {}
+import PouchDB from "pouchdb";
+export default {
+  data: function () {
+    return {
+      movies: "",
+      rows: "",
+      newTitle: "",
+      newId: "",
+    };
+  },
+  methods: {
+    add: function () {
+      this.movies
+        .put({
+          _id: this.newId,
+          title: this.newTitle,
+        })
+        .then(function (response) {
+          console.log("Success", response);
+        })
+        .then(function (err) {
+          console.log("Error", err);
+        });
+    },
+    test: function () {
+      console.log(this.rows);
+    },
+  },
+  mounted: function () {
+    let self = this;
+    this.movies = new PouchDB("Movies");
+    this.movies.allDocs({ include_docs: true }).then(function (docs) {
+      let data = [];
+      docs.rows.forEach(function (doc) {
+        data.push(doc);
+      });
+      self.rows = data;
+    });
+  },
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
